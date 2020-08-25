999ae6c4bf5bfb5d8a470bd855f67b82acf407f3
Gean Kaminski <geankaminski@gmail.com>
2020-08-24 21:41:37 -0300

    SPA 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay,
-    CardTitle } from 'reactstrap';
+        CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderMenuItem ({dish, onClick}) {
         return (
             <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
+                <Link to={`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
             </Card>
         );
     }
 
     const Menu = (props) => {
-
         const menu = props.dishes.map((dish) => {
             return (
                 <div className="col-12 col-md-5 m-1"  key={dish.id}>
@@ -26,7 +28,17 @@ import { Card, CardImg, CardImgOverlay,
         return (
             <div className="container">
                 <div className="row">
-                    {menu}
+                    <Breadcrumb>
+                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Menu</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>Menu</h3>
+                            <hr />
+                        </div>                
+                    </div>
+                    <div className="row">
+                        {menu}
                 </div>
             </div>
         );
